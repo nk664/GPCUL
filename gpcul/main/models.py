31ae0b9cf99f7b964683818520a915ed86964a40
nk664 <knaresh96387@gmail.com>
2026-04-27 22:20:02 +0530

    'registration models are under development '

--- gpcul/main/models.py
+++ gpcul/main/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Student(models.model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+
+
+    # personal information
+    
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    dob = models.DateField()
+    gender = models.CharField(max_length=10)
+    mobile = models.CharField(max_length=15)
+    photo = models.ImageField(upload_to='profile_photo/',null = True, blank=True)
+    
+
+
+    #Academics information
+    
+
+    enrollment = models.CharField(max_length=60)
+    admission_year = models.CharField(max_length=10)
+    course = models.CharField(max_length=50)
+    semester = models.CharField(max_length=10)
+    address = models.TextField()
+    
+
+
+    #login setup 
+    
+
+    first_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
